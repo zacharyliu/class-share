03d93e749b9147e1049d093e69d6f9ec708a43f1
Zachary Liu <liu.zachary@gmail.com>
2012-08-20 12:08:48 -1000

    Don't show the current user in search.

--- application/models/datamod.php
+++ application/models/datamod.php
@@ -117,7 +117,8 @@ class Datamod extends CI_Model {
     
     public function search($q) {
         $q = $this->db->escape_like_str($q);
-        $query = $this->db->query('SELECT * FROM (SELECT id, CONCAT(name, ", ", teacher) AS body, "class" AS type FROM classes UNION ALL SELECT id, name AS body, "person" AS type FROM people) AS search WHERE body LIKE "%' . $q . '%" GROUP BY body');
+        $user_id = $this->db->escape($this->session->userdata('id'));
+        $query = $this->db->query('SELECT * FROM (SELECT id, CONCAT(name, ", ", teacher) AS body, "class" AS type FROM classes UNION ALL SELECT id, name AS body, "person" AS type FROM people WHERE id != ' . $user_id . ') AS search WHERE body LIKE "%' . $q . '%" GROUP BY body');
         $result = $query->result();
         $result_chunk = array_chunk($result, 5);
         if (count($result_chunk) > 0) {
