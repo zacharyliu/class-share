d4522565a7b71a4456421568f89a0aef4086ed5b
Zachary Liu <liu.zachary@gmail.com>
2012-08-19 17:54:42 -1000

    Visual tweaks to the login message and changed the index_page config value for mod_rewrite usage.

--- application/views/login.php
+++ application/views/login.php
@@ -2,9 +2,23 @@
 <html>
     <head>
         <title>Class Schedule - Login</title>
+        <style type="text/css">
+            div {
+                text-align: center;
+                font-size: 12pt;
+                width: 500px;
+                margin: 0 auto;
+                font-family: 'Tahoma', sans-serif;
+            }
+            div a {
+                font-size: 14pt;
+            }
+        </style>
     </head>
     <body>
-        Hello! In order to access this site, you must log in using your school Google account. Clicking the login link below will take you to the Google account login page, where you will complete the login process. Be sure to use your school Google account. Afterwards, you will be redirected back to this site. Your password will not be transmitted to this site.<br />
-        <a href="<?=site_url('login')?>">Login</a>
+        <div>
+            Hello! In order to access this site, you must log in using your school Google account. Clicking the login link below will take you to the Google account login page, where you will complete the login process. Be sure to use your school Google account. Afterwards, you will be redirected back to this site. Your password will not be transmitted to this site.<br /><br />
+            <a href="<?=site_url('login')?>">Login</a>
+        </div>
     </body>
 </html>
