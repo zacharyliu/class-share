993b56a7d57a9f284d6ff655a773710f9e66779d
Zachary Liu <liu.zachary@gmail.com>
2014-08-06 20:08:29 -0400

    Add import instructions

--- application/views/import.php
+++ application/views/import.php
@@ -2,8 +2,9 @@
 <html>
 <body>
 
+<p>Upload a saved HTML file (right-click -> Save as... -> Webpage, HTML Only) of your PowerSchool homepage below. An automated script will parse and import your schedule from this file.</p>
+
 <form action="<?=site_url('import/submit')?>" method="post" enctype="multipart/form-data">
-<label for="file">Filename:</label>
 <input type="file" name="file" id="file" /> 
 <br />
 <input type="submit" name="submit" value="Submit" />
